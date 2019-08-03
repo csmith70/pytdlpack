8c1b639b145b3cce5c84eb96b77bd3f7642f3d0e
Eric Engle <eengl@users.noreply.github.com>
2019-08-02 23:10:26 -0400

    Creating gh-pages.

--- create_docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pdoc --html --html-no-source --overwrite --html-dir 'docs' pytdlpack
-#pdoc --html --html-no-source --overwrite --html-dir 'docs' TdlpackIO
--- pytdlpack/__init__.py
+++
@@ -1,8 +0,0 @@
-# init for pytdlpack package
-from ._pytdlpack import *
-from ._pytdlpack import __doc__,__pdoc__
-
-from .version import version as __version__
-
-__all__ = ['__version__','TdlpackFile','TdlpackRecord','TdlpackStationRecord','TdlpackTrailerRecord',
-           'open','create_grid_definition',]
